09ba69b9ce1f8c6d14d1f0f158982e8fbdc09558
Gasilov Danil <gasilov.d@roistat.com>
2024-11-10 17:39:19 +0300

    причесал код

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -17,13 +17,7 @@ class JobController extends Controller
     {
 
         // Если надо, можно добавить аренду вручную, у которой срок сдачи меньше 24 часов для проверки, просто раскоментировать
-        // $rental = Rental::create([
-        //     'user_id' => 1,       // Замените на существующий user_id
-        //     'book_id' => 1,       // Замените на существующий book_id
-        //     'rented_at' => "2024-11-09 00:00:00",
-        //     'due_date' => "2024-11-09 00:10:00",  // Срок истекает через 5 часов
-        //     'returned_at' => null,
-        // ]);
+
 
         $now = Carbon::now();
         $dueSoon = $now->copy()->addDay();
